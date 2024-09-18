fae5db87cbac0440557587e3abf3a56374220ee1
Pablo Gomez Barreiro <P.GomezBarreiro@kew.org>
2024-09-18 09:14:54 +0100

    coolbear update

---
+++ R/coolbear.R
@@ -0,0 +1,43 @@
+#' Obtain T50 values
+#'
+#' Obtain T50 values using Coolbear et al. (1984) formula modified by Farooq et al.(2005). Vector containing scoring days must have same length than vector containing cumulative germination data
+#'
+#' @param day A vector containing scoring days
+#' @param cumulative A vector containing the cumulative germination for each day
+#' @param n Number of viable seeds in the Petri dish (Total sown seeds - empty seeds)
+#'
+#' @return t50 value in days
+#' @export
+#'
+#' @examples
+#' coolbear(seq(1,10,1),c(0,0,0,0,0,6,12,15,18,20),20)
+#'
+#'
+coolbear <- function(day = "vector containing scoring dates",
+                     cumulative = "vector containing cumulative germination for each day",
+                     n ="total number of viable seeds") {
+  if (max(cumulative)<n/2){ #If germination is under 50%, is not possible to calculate T50
+    t50<-NA
+    return(t50)
+  }
+  if (max(cumulative)==n/2){ #If maximum germination is 50% of viable seeds, T50 is time to reach maximum germination
+    t50<-day[min(which(cumulative==n/2))]
+    return(t50)
+  }
+  if (any(n/2==cumulative)){ #If exactly 50% germination is reached in a day, T50 is that day. Same formula than before... merge eventually
+    t50<-day[min(which(cumulative==n/2))]
+    return(t50)
+  }
+  if (cumulative[[1]]>n/2){ #If the first cumulative germination is already bigger than n/2, T50 is (0+first counting day)/2
+    t50<-(0+day[[1]])/2
+    return(t50)
+  }
+  else{
+    ni<-cumulative[sum(cumulative<=n/2)]
+    nj<-cumulative[sum(cumulative<=n/2)+1]
+    ti<-day[sum(cumulative<=n/2)]
+    tj<-day[sum(cumulative<=n/2)+1]
+    t50<- ti+(((n/2-ni)*(ti-tj))/(ni-nj))
+    return(t50)
+  }
+}
--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # seedTools
 
+<!-- badges: start -->
+  [![R-CMD-check](https://github.com/pgomba/seedTools/actions/workflows/R-CMD-check.yaml/badge.svg)](https://github.com/pgomba/seedTools/actions/workflows/R-CMD-check.yaml)
+  <!-- badges: end -->
+
 An R package with a collection of functions to simplify seed biology related tasks
 
 ![](images/seedTools2.png)
